bc3bf40ff5be2a1a28eb08244f5ff5e9c41b6e42
Biniyamseid <Biniyamseid3@gmail.com>
2024-04-26 17:23:46 +0300

    refactor

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')), 
+    # path('', lambda request: redirect('/api/users/', permanent=True)),  # Redirect base URL to /api/users/
     path('accounts/', include('allauth.urls'))
 ]
 
